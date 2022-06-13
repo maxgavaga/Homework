c9fc1530abf44c02e365ba3e48554315fa33621d
maxgavaga <ferrarif138@yandex.ru>
2022-06-13 09:15:37 +0300

    Added .gitignore

---
+++ Task 1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдает, какое число большее, а какое меньшее.
+
+  int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} это наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} это наибольшее число ", b);
+ 
+        }
+
+        if (a < b)
+ 
+        {
+ 
+            Console.WriteLine("{0} это наименьшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} это наименьшее число ", b);
+ 
+        }
+
