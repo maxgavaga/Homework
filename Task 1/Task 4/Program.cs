dbfe4cf6ede24f43717a6670935ae002f6f63ee7
maxgavaga <ferrarif138@yandex.ru>
2022-06-13 14:51:00 +0300

    Task 4

---
+++ Task 1/Task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от одного до N.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+for (int i = a; i <= b; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write( i + " ");
+    }
+}
